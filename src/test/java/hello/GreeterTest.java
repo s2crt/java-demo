632706117f0483333f5f76967e2b994d36c063ac
mahewitson <78550546+mahewitson@users.noreply.github.com>
2021-02-26 11:52:39 +0000

    Update GreeterTest.java

--- src/test/java/hello/GreeterTest.java
+++ src/test/java/hello/GreeterTest.java
@@ -11,7 +11,7 @@ public class GreeterTest {
 
   @Test
   public void greeterSaysHello() {
-    assertThat(greeter.sayHello(), containsString("Hi"));
+    assertThat(greeter.sayHello(), containsString("Hello"));
   }
 
 }
